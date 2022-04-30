ce80c91b5a8143b3be7988b80b9d32d27604316e
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-04-30 20:33:34 -0300

    Removidos controller e classe criados por padrão

---
+++ src/ANCD.API/Controllers/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using ANCD.Application.Mediator;
+using ANCD.Application.Messages.CommandsQueries;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ANCD.API.Controllers
+{
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        protected IMediator _mediator;
+
+        protected BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        protected IActionResult DefaultResponse(CommandResult result, HttpStatusCode statusCode)
+        {
+            if (result.IsSuccess is false)
+                return BadRequest(result.Errors);
+
+            return ApiDefaultResponse<object>(null, statusCode);
+        }
+
+        private IActionResult ApiDefaultResponse<T>(T result, HttpStatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                case HttpStatusCode.OK:
+                    return result is null ? Ok() : Ok(result);
+                case HttpStatusCode.Created:
+                    return new ObjectResult(result) { StatusCode = (int)HttpStatusCode.Created };
+                case HttpStatusCode.NoContent:
+                    return NoContent();
+                default:
+                    return result is null ? Ok() : Ok(result);
+            }
+        }
+    }
+}
