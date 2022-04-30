db336040560df2afb90e31306547d69c0250ec45
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-04-30 20:32:26 -0300

    Versionamento e documentação
    Configurado o uso do Swagger e o versionamento da API.

---
+++ src/ANCD.API/Extensions/VersionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ANCD.API.Extensions
+{
+    public static class VersionExtensions
+    {
+        public static IServiceCollection ConfigureVersion(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            });
+
+            return services;
+        }
+    }
+}
--- src/ANCD.API/Program.cs
+++ src/ANCD.API/Program.cs
@@ -1,19 +1,18 @@
+using ANCD.API.Extensions;
 using ANCD.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfig();
 builder.Services.AddInfraConfiguration();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerConfig();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
